10efc9e1ab30b2480f516a5bb9d08d6c8d2c1fb7
Vinícius Stencel Canto Silva <stencel.vinicius@gmail.com>
2025-04-27 21:18:05 -0300

    First Commit

---
+++ src/main/java/br/com/unicuritiba/apilivros/ApiLivrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.unicuritiba.apilivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLivrosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiLivrosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/unicuritiba/apilivros/ApiLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unicuritiba.apilivros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLivrosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
